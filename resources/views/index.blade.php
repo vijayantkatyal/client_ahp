f3ca16db832231eb37a5717497b099f5d5dd68d3
Vijayant Katyal <1795103+vijayantkatyal@users.noreply.github.com>
2024-03-31 02:11:29 +0530

    [Updated] mission

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -179,8 +179,12 @@
 		<div class="row">
 			<div class="col-lg-7 ">
 				<h3>Our Mission</h3>
-				<p class="mt-4 res-margin">Aliquam erat volutpat In id fermentum augue, ut pellentesque leo. Maecena Aliquam erat volutpat In id fermentum augue, ut pellentesque leo. Maecenas at arcu risus. Donec commodo sodales ex, scelerisque laoreet nibh hendrerit id. In aliquet magna nec lobortis maximus. Etiam rhoncus leo a dolor placerat, nec elementum ipsum convall.</p>
-				<p>Nec elementum ipsum convall. Aliquam erat volutpat In id fermentum augue, ut pellentesque leo. Maecen Etiam rhoncus leo a dolor placerat, nec elementum ipsum convall Maecenas at arcu risus scelerisque laoree.</p>
+				<p class="mt-4 res-margin">
+                    Alberta Hindi Parishad was founded in 1985 with main objective to provide regular classes to teach Hindi systematically. Hindi Vidyalaya was started in 1987 at the University of Alberta with the help of (late) Professor Emeritus Dr. Ambikeshwar Sharma. Alberta Hindi Parishad chose the books prepared and published by National Council of Educational Research and Training (NCERT)-Baal Bharati is used in the central school system throughout India, as the curriculum to structure the classes. Experienced teachers provide basic training and guidance to the volunteer teachers. Hindi Vidyalaya conducts five classes (levels 1 to 5) for children from 5 to 15 years old.
+                </p>
+                <p>
+                    There are separate classes for adults where basic reading, writing and speaking is taught, followed by conversation class, where emphasis is on Hindi speaking and a class of grammar. Students participate in many programs like Holi, Independence Day, Kavi Sammelan, Hindi Divas, Antakshari, field trips and annual picnic. These programs are organized by Hindi Parishad. Students and members are encouraged to submit articles, poems and short stories our annual publication MEDHAVANI and newsletter KRITI.
+                </p>
 				<a href="contact.php" class="btn btn-secondary ">Contact us</a>
 			</div>
 			<!-- /col-lg -->
